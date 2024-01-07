9ff92a399056548b7582ce22d4ed0bf87344096e
ArjunB126 <arjunbajare86@gmail.com>
2024-01-07 23:20:37 +0530

    Initial_Commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import os
+import vertexai
+from vertexai.language_models import TextGenerationModel
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "... Add Path ..."
+PROJECT_ID = "total-entity-409908"  
+LOCATION = "us-central1" 
+
