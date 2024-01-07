4960ba2e9835c44b0263fb8f4e99de8ef9e572dd
ArjunB126 <arjunbajare86@gmail.com>
2024-01-07 23:23:09 +0530

    Initial_Updates

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ from vertexai.language_models import TextGenerationModel
 PROJECT_ID = "total-entity-409908"  
 LOCATION = "us-central1" 
 
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+def create_session():
+    chat_model = vertexai.language_models.ChatModel.from_pretrained("chat-bison@001")
+    chat = chat_model.start_chat()
+    return chat
